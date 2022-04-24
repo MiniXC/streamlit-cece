9bf1bec6391b279faf2035e0bf8d91ba22e37915
Christoph Minixhofer <christoph.minixhofer@gmail.com>
2022-04-25 00:21:00 +0100

    initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,26 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import pymongo
+from time import sleep
 
-"""
-# Welcome to Streamlit!
+pwd = st.secrets["DB_PASSWORD"]
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+client = pymongo.MongoClient(f"mongodb+srv://root:{pwd}@cece.ldq20.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.cece
+messages = db.messages
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+st.header("Cece Printer Interface")
+st.write("This app writes a message to a database, then waits for 5 seconds for it to be printed, then deletes it again.")
+to = st.selectbox('send message to',['stouf'])
+text = st.text_input('message')
+if st.button('send'):
+    msg_result = messages.insert_one({"for": to, "text": text, "printed": False})
+    st.success("sent message to printer")
+    _id = msg_result.inserted_id
+    sleep(5)
+    msg_printed = messages.find_one({"_id": _id})
+    if msg_printed["printed"]:
+        st.success("successfully printed message")
+    else:
+        st.error("printing failed, printer might be offline")
+    messages.delete_one({"_id": _id})
+    st.info("message deleted")
